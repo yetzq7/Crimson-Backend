d3487a2a931b40e17cde47cca4c01a8b8db4bb72
yetzq7 <yetagosto12@icloud.com>
2026-06-06 02:51:05 -0400

    Update keychain.js

--- src/utils/keychain.js
+++ src/utils/keychain.js
@@ -3,7 +3,7 @@ const app = express();
 
 const keychain = require("../keychain/keychain.json");
 
-app.get('/fortnite/api/storefront/v2/keychain', (req, res) => {
+app.get('/fortnite/api/storefront/v2/keychain', async (req, res) => {
     return res.status(200).send(keychain);
 })
 
